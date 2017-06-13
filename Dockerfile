35f65316b4a2895b6f48ff4632e56f1990bdcef7
Sofiia Andriichenko <sandriichenko@mirantis.com>
2017-06-13 08:45:24 +0000

    Removed ironic and ceilimeter plugins

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,8 @@ USER root
 RUN git clone https://git.openstack.org/openstack/tempest -b 15.0.0 && \
     pip install tempest==15.0.0 && \
     pip install ddt==1.0.1 && \
-    git clone https://github.com/openstack/ironic.git && \
     git clone https://github.com/openstack/designate-tempest-plugin.git && \
-    git clone https://github.com/openstack/ceilometer.git && \
-    pip install -r ironic/test-requirements.txt && \
-    pip install -r designate-tempest-plugin/test-requirements.txt && \
-    pip install -r ceilometer/test-requirements.txt
+    pip install -r designate-tempest-plugin/test-requirements.txt
 
 WORKDIR /home/rally
 
--- run_tempest.sh
+++ run_tempest.sh
@@ -5,12 +5,10 @@ source /home/rally/$SOURCE_FILE
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 15.0.0 --system-wide
-rally verify add-verifier-ext --source /var/lib/ironic
-rally verify add-verifier-ext --source /var/lib/ceilometer
 rally verify add-verifier-ext --source /var/lib/designate-tempest-plugin
 rally verify configure-verifier --extend /var/lib/tempest_conf/$TEMPEST_CONF
 rally verify configure-verifier --show 
-report='report_'`date +%F_%H-%M`
+report='report'_`date +%F_%H-%M`
 rally verify start --skip-list /var/lib/skip_lists/$SKIP_LIST $CUSTOM
 rally verify report --type junit-xml --to $report.xml
 rally verify report --type html --to $report.html 

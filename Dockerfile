7e6b4b55c11cdebbe9ee7c6a03d608be7a32f99b
Sofiia Andriichenko <sandriichenko@mirantis.com>
2017-06-15 13:37:48 +0000

    Changed min_compute_nodes variable

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ MAINTAINER Sofiia Andriichenko <sandriichenko@mirantis.com>
 
 WORKDIR /var/lib
 USER root
-RUN git clone https://git.openstack.org/openstack/tempest -b 15.0.0 && \
-    pip install tempest==15.0.0
+RUN git clone https://git.openstack.org/openstack/tempest && \
+    pip install tempest && \
+    git clone https://github.com/openstack/ironic.git && \
+    pip install -r ironic/test-requirements.txt
+ 
+
 
 WORKDIR /home/rally
 
--- run_tempest.sh
+++ run_tempest.sh
@@ -7,7 +7,7 @@ rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 15.0.0 --system-wide
 rally verify configure-verifier --extend /var/lib/lvm_mcp.conf
 rally verify configure-verifier --show
-rally verify start --skip-list /var/lib/mcp_skip.list --pattern set=$SET
-report='report_'$SET-`date +%F_%H-%M`
+rally verify start --skip-list /var/lib/mcp_skip.list $CUSTOM
+report='report_'`date +%F_%H-%M`
 rally verify report --type junit-xml --to $report.xml
 rally verify report --type html --to $report.html

ab85288afeccfe5055fab84c5984fa03ce7d9a8c
sandriichenko <sandriichenko@mirantis.com>
2017-07-05 14:54:35 +0300

    Update run_tempest.sh

--- run_tempest.sh
+++ run_tempest.sh
@@ -9,7 +9,7 @@ rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --system-wide
 rally verify add-verifier-ext --source /var/lib/designate-tempest-plugin
 rally verify add-verifier-ext --source /var/lib/ironic
-#rally verify add-verifier-ext --source /var/lib/murano
+rally verify add-verifier-ext --source /var/lib/murano
 rally verify configure-verifier --extend /var/lib/lvm_mcp.conf 
 rally verify configure-verifier --show | tee -a $log
 if [ $SET ]

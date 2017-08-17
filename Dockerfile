1ff0f1d651c66282f92f2315889dfdd0572d5058
sandriichenko <sandriichenko@mirantis.com>
2017-08-17 13:53:58 +0000

    Changed for newton

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,15 @@
-FROM rallyforge/rally:0.9.0
+FROM rallyforge/rally:0.9.1
 MAINTAINER Sofiia Andriichenko <sandriichenko@mirantis.com>
 
 WORKDIR /var/lib
 USER root
 RUN git clone https://git.openstack.org/openstack/tempest && \
-    pip install tempest && \
-    git clone https://github.com/openstack/ironic.git && \
-    pip install -r ironic/test-requirements.txt && \
-    pip install ddt==1.0.1 && \
-    git clone https://github.com/openstack/designate-tempest-plugin.git && \
-    pip install -r designate-tempest-plugin/test-requirements.txt && \
-    git clone https://github.com/openstack/murano.git && \
-    pip install -r murano/test-requirements.txt
-
+    pip install tempest 
 
 WORKDIR /home/rally
 
 COPY mcp_skip.list /var/lib/mcp_skip.list
-COPY lvm_mcp.conf /var/lib/lvm_mcp.conf
+COPY lvm_mcp.conf /var/lib/lvm_mcp_newton.conf
 COPY run_tempest.sh /usr/bin/run-tempest
 
 ENV SOURCE_FILE keystonercv3
--- run_tempest.sh
+++ run_tempest.sh
@@ -2,24 +2,12 @@
 
 source /home/rally/$SOURCE_FILE
 
-log_dir="${LOG_DIR:-/home/rally/rally_reports/}"
-mkdir -p $log_dir
-
-report='report_'$SET'_'`date +%F_%H-%M`
-log=$log_dir/$report.log
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
-rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --system-wide
-rally verify add-verifier-ext --source /var/lib/designate-tempest-plugin
-rally verify add-verifier-ext --source /var/lib/ironic
-#rally verify add-verifier-ext --source /var/lib/murano
-rally verify configure-verifier --extend /var/lib/lvm_mcp.conf 
-rally verify configure-verifier --show | tee -a $log
-if [ $SET ]
-then
-    rally verify start --skip-list /var/lib/mcp_skip.list --pattern set=$SET | tee -a  $log
-else
-    rally verify start --skip-list /var/lib/mcp_skip.list $CUSTOM  | tee -a  $log
-fi
-rally verify report --type junit-xml --to $log_dir/$report.xml
-rally verify report --type html --to $log_dir/$report.html
+rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version ${TEMPEST_VERIFIER_VERSION:-15.0.0} ${TEMPEST_VERIFIER_CUSTOM:---system-wide}
+rally verify configure-verifier --extend /var/lib/lvm_mcp_newton.conf
+rally verify configure-verifier --show 
+report='report'_`date +%F_%H-%M`
+rally verify start --skip-list /var/lib/mcp_skip.list $CUSTOM
+rally verify report --type junit-xml --to $report.xml
+rally verify report --type html --to $report.html
